13353862889aaeff98f1a9d00371a3273eab7d92
ShinRunner1st <shinrunner1st@gmail.com>
2023-10-01 12:49:12 +0700

    d

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="660612148"
+                fullName="Norrawich Sombutnan"
                 year="2023"
               />
             </Container>
